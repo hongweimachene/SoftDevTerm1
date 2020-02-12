f17e794d61e6219f9a03c073be30207c2819e572
hongweimachene <hchen00@stuy.edu>
2020-02-12 16:39:33 -0500

    done

--- 07_canvas-anim/anim.js
+++ 07_canvas-anim/anim.js
@@ -1,42 +1,63 @@
+// Hong Wei Chen
+// SoftDev2 pd9
+// K07 -- They lock us in the tower whenever we get caught ...which is often
+// 2020-02-13
+
 var c = document.getElementById("screen")
 var ctx = c.getContext("2d")
 
+// declare vars
 var request;
 var start = document.getElementById("start");
 var stop = document.getElementById("stop");
-var state = 0;
-var rad;
+// radius of circle
+var rad = 1;
+// expand or contract
 var expand = 0;
 
+// animate button
 var play = function(e) {
-  rad = 1;
-  
-  request = requestAnimationFrame(draw)
+  // if there are no existing requests, request the next frame
+  if (!request){
+    request = window.requestAnimationFrame(draw);
+  }
 }
 
+// stop button
 var halt = function(e) {
-  cancelAnimationFrame(request)
+  // if request exists, cancel the request
+  if (request) {
+    window.cancelAnimationFrame(request)
+    request = undefined; // resets request so it doesnt exist
+  }
 }
 
 var draw = function(e) {
-  request = requestAnimationFrame(draw)
-  ctx.fillStyle = '000000';
+  request = undefined; // reset request to get next frame
+  ctx.clearRect(0,0,c.width,c.height); // clear canvas
+
+  //draw circle
+  ctx.fillStyle = '#add8e6';
   ctx.beginPath();
   ctx.ellipse(c.width / 2,c.height / 2,rad,rad,0,0,Math.PI * 2);
   ctx.fill();
+
+  // checks if circle should expand or contract
   if (rad >= c.width/2) {
     expand = 0
-  } else {
+  }
+  if (rad <= 1) {
     expand = 1
   }
-  if (expand = 1) {
-    rad += 1
+  if (expand == 1) {
+    rad += 2
   } else {
-    rad -= 1
+    rad -= 2
   }
+
+  // call animation button
+  play();
 }
 
-request = requestAnimationFrame(draw)
-
-start.addEventListener('click', play)
+start.addEventListener('click', play);
 stop.addEventListener('click', halt);
